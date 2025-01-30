4c43040b4d557e2ef5d0fad570ccf8d3e0c01ada
polozovaanastasia <polozova.anastasia@gmail.com>
2025-01-30 12:44:55 +0500

    refactor: split mixed Home into UI HomeLayout and compositional Home.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,92 +20,101 @@ function Home() {
         onPlayersTimeOverHandler,
     } = useGameState(playersCount);
     return (
-        <div className={styles["app"]}>
-            <Header />
-            <main className={styles["app-content"]}>
-                <GameTitle playersCount={playersCount} />
-                <GameInfo
-                    currentMove={currentMove}
-                    playersCount={playersCount}
-                    players={[
-                        {
-                            id: 2,
-                            name: "Alex",
-                            rating: 220,
-                            symbol: GAME_SYMBOL.ZERO,
-                            time: 30000,
-                            avatar: "/images/avatar2.png",
-                        },
-                        {
-                            id: 3,
-                            name: "Soomi",
-                            rating: 112233445566778899,
-                            symbol: GAME_SYMBOL.CROSS,
-                            time: 30,
-                            avatar: "/images/avatar.png",
-                        },
-                        {
-                            id: 1,
-                            name: "Polozova",
-                            rating: 230,
-                            symbol: GAME_SYMBOL.TRIANGLE,
-                            time: 30,
-                            avatar: "/images/avatar.png",
-                        },
-                        {
-                            id: 4,
-                            name: "Viliam Dacarad",
-                            rating: 5220,
-                            symbol: GAME_SYMBOL.SQUARE,
-                            time: 30,
-                            avatar: "/images/avatar2.png",
-                        },
-                    ]}
-                    isWinner={!!winnerSequence}
-                    onPlayersTimeOverHandler={onPlayersTimeOverHandler}
-                    className="container-margins"
-                />
-                <UIModal
-                    // isOpen={!!winnerSequence}
-                    isOpen={true}
-                    onClose={() => console.log("Закрываю окно")}
-                >
+        <HomeLayout header={<Header />}>
+            <GameTitle playersCount={playersCount} />
+            <GameInfo
+                currentMove={currentMove}
+                playersCount={playersCount}
+                players={[
                     {
-                        <>
-                            <UIModal.Header>{"Игра завершена!"}</UIModal.Header>
-                            <UIModal.Body>
-                                {<div>{"Modal Body"}</div>}
-                            </UIModal.Body>
-                            <UIModal.Footer>
-                                <UIButton
-                                    variant="outline"
-                                    size={SIZES.MEDIUM}
-                                    onClick={() => {}}
-                                >
-                                    {"Вернуться"}
-                                </UIButton>
-                                <UIButton
-                                    variant="primary"
-                                    size={SIZES.MEDIUM}
-                                    onClick={() => {}}
-                                >
-                                    {"Играть снова"}
-                                </UIButton>
-                            </UIModal.Footer>
-                        </>
-                    }
-                </UIModal>
-                <GameField
-                    cells={cells}
-                    currentMove={currentMove}
-                    nextMove={nextMove}
-                    winnerSequence={winnerSequence}
-                    onCellClickHandler={onCellClickHandler}
-                />
-            </main>
+                        id: 2,
+                        name: "Alex",
+                        rating: 220,
+                        symbol: GAME_SYMBOL.ZERO,
+                        time: 30000,
+                        avatar: "/images/avatar2.png",
+                    },
+                    {
+                        id: 3,
+                        name: "Soomi",
+                        rating: 112233445566778899,
+                        symbol: GAME_SYMBOL.CROSS,
+                        time: 30,
+                        avatar: "/images/avatar.png",
+                    },
+                    {
+                        id: 1,
+                        name: "Polozova",
+                        rating: 230,
+                        symbol: GAME_SYMBOL.TRIANGLE,
+                        time: 30,
+                        avatar: "/images/avatar.png",
+                    },
+                    {
+                        id: 4,
+                        name: "Viliam Dacarad",
+                        rating: 5220,
+                        symbol: GAME_SYMBOL.SQUARE,
+                        time: 30,
+                        avatar: "/images/avatar2.png",
+                    },
+                ]}
+                isWinner={!!winnerSequence}
+                onPlayersTimeOverHandler={onPlayersTimeOverHandler}
+                className="container-margins"
+            />
+            <UIModal
+                // isOpen={!!winnerSequence}
+                isOpen={true}
+                onClose={() => console.log("Закрываю окно")}
+            >
+                {
+                    <>
+                        <UIModal.Header>{"Игра завершена!"}</UIModal.Header>
+                        <UIModal.Body>{<div>{"Modal Body"}</div>}</UIModal.Body>
+                        <UIModal.Footer>
+                            <UIButton
+                                variant="outline"
+                                size={SIZES.MEDIUM}
+                                onClick={() => {}}
+                            >
+                                {"Вернуться"}
+                            </UIButton>
+                            <UIButton
+                                variant="primary"
+                                size={SIZES.MEDIUM}
+                                onClick={() => {}}
+                            >
+                                {"Играть снова"}
+                            </UIButton>
+                        </UIModal.Footer>
+                    </>
+                }
+            </UIModal>
+            <GameField
+                cells={cells}
+                currentMove={currentMove}
+                nextMove={nextMove}
+                winnerSequence={winnerSequence}
+                onCellClickHandler={onCellClickHandler}
+            />
             <div id="modals"></div>
-        </div>
+        </HomeLayout>
     );
 }
 
 export default Home;
+
+type HomeLayoutPropsType = {
+    header: React.ReactNode;
+    children: React.ReactNode;
+};
+
+function HomeLayout({ header, children }: HomeLayoutPropsType) {
+    return (
+        <div className={styles["app"]}>
+            {header}
+            <main className={styles["app-content"]}>{children}</main>
+        </div>
+    );
+}
